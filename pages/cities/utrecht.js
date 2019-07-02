8a6c0820beb546ba4781dc203f9d56671ee49831
Joachim Roeleveld <joachimroeleveld@gmail.com>
2019-07-02 10:53:34 +0200

    Fixup

--- pages/cities/utrecht.js
+++ pages/cities/utrecht.js
@@ -169,6 +169,7 @@ UtrechtCityPage.getInitialProps = async ctx => {
       limit: 8,
       query: {
         pageSlug,
+        tagged: true,
       },
     })).results,
     blogs: await getPostsFiltered(`tags:${pageSlug.replace('/', '-')}`, {
